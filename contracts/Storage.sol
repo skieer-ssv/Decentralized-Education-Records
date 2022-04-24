09dceedb338bf31c694c529582485af9b60b19d3
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2022-04-24 20:20:30 +0530

    feat: one contract per student

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,18 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Storage{
-struct CertificateData{
-string  ipfsHash;
-string studentId;
+contract Student{
+struct studentData{
+string  name;
+string id;
 }
-CertificateData certificateData;
 
-constructor (string memory ipfsHash,string memory studentId ){
-    certificateData.ipfsHash = ipfsHash;
-    certificateData.studentId = studentId;
+struct certificate{
+    string title;
+    string ipfsHash;
+//TODO: add issuing institute and date
 }
-function  getIpfsHash() public view returns (CertificateData memory){
-    return certificateData;
+studentData s;
+certificate[] c;
+
+constructor (string memory name,string memory studentId  ){
+    s.name = name;
+    s.id = studentId;
+}
+
+function addCertificate(string memory title, string memory ipfsHash)public {
+    c.push(certificate(title,ipfsHash));
+}
+function getCertificateData() public view returns(certificate[] memory){
+    return c;
+}
+function  getStudentData() public view returns (studentData memory){
+    return s;
 }
 }
